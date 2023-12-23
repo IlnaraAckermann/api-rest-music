172d89819875882e27f8f6148b014317ffbcf86c
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2023-12-23 19:49:22 -0300

    Update: Atualizada a classe Music

--- src/main/kotlin/ackermann/ilnara/apirest/repository/MusicRepository.kt
+++ src/main/kotlin/ackermann/ilnara/apirest/repository/MusicRepository.kt
@@ -2,6 +2,8 @@ package ackermann.ilnara.apirest.repository
 
 import ackermann.ilnara.apirest.model.Music
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface MusicRepository: JpaRepository<Music, Long> {
 }
