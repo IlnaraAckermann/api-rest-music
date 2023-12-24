e0120d629c3f5910362bb1b6e2ed116351bde46b
Ilnara Ackermann <ilnara.ackermann@hotmail.com>
2023-12-24 01:10:20 -0300

    feat: Adiciona MusicRepository

--- src/main/kotlin/ackermann/ilnara/apirest/repository/MusicRepository.kt
+++ src/main/kotlin/ackermann/ilnara/apirest/repository/MusicRepository.kt
@@ -1,9 +1,16 @@
 package ackermann.ilnara.apirest.repository
 
 import ackermann.ilnara.apirest.model.Music
+import ackermann.ilnara.apirest.queryProjections.AlbumProjection
+import ackermann.ilnara.apirest.queryProjections.MusicPhotosProjection
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
 interface MusicRepository: JpaRepository<Music, Long> {
+    @Query(value = "SELECT URL, TUMBNAIL_URL FROM MUSIC WHERE ALBUM_ID = ?1", nativeQuery = true)
+    fun findAllPhotosByAlbumId(id: Long): List<MusicPhotosProjection>
+    @Query(value = "SELECT DISTINCT album_id FROM Music", nativeQuery = true)
+    fun findAllAlbumId(): List<AlbumProjection>
 }
